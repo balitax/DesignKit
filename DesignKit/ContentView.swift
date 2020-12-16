8b954614722abfd0dfbc2e40ca2aaf1898445262
Agus Cahyono <aguscahyono@roomme.space>
2020-12-16 10:33:02 +0700

    Initial Commit

---
+++ DesignKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignKit
+//
+//  Created by Agus Cahyono on 16/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesignKit/DesignKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignKitApp.swift
+//  DesignKit
+//
+//  Created by Agus Cahyono on 16/12/20.
+//
+
+import SwiftUI
+
+@main
+struct DesignKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
